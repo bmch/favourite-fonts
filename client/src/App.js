83e32e62b092eb6b0c6d488f63a4c57e8a592291
Brendan McHugh <brendanmchugh12@gmail.com>
2020-01-06 18:59:34 +0100

    css and js create react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header-container">
+        <div className="header-left">Google Fonts </div>
+
+        <div className="header-right">
+          <div className="menu-item">CATALOG</div>
+          <div className="menu-item">FEATURED</div>
+          <div className="menu-item">ARTICLES</div>
+          <div className="menu-item">ABOUT</div>
+        </div>
+      </div>
+
+      <nav>Nav</nav>
     </div>
   );
 }
